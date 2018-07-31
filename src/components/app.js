88666f491ccebd4a34d843198b8d256f2fe2896c
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 10:54:36 -0600

    made count down count the date you select

--- src/components/app.js
+++ src/components/app.js
@@ -13,9 +13,47 @@ export default class App extends Component {
 
     this.state = {
       active: false,
-      startDate: moment
+      startDate: moment()
     }
   }
+
+  handleChange =function(date) {
+    this.setState({
+        startDate: date
+    });
+  }.bind(this)
+
+  handleGenerate = function() {
+    this.setState({ active: true })
+    //Set the date we-re counting down to
+    var countDownDate = this.state.startDate.getTime();
+
+    //update date every second
+    var x = setInterval(function(){
+
+      //get todays date and time
+      var now =new Date().getTime();
+
+      //Find the distance between now an the count down date
+      var distance = countDownDate - now;
+
+      //time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000*60*60*24));
+      var hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+      var minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+      var seconds =Math.floor((distance % ( 1000*60))/1000);
+
+      //output the result in an element with id="demo"
+      const time = days + "d " + hours + "h " +minutes + "m " + seconds+ "s ";
+      console.log(time)
+
+      //If the countdown is over, write some text
+      if (distance < 0) {
+        clearTnterval(x);
+        // document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  }.bind(this)
 
   renderItems(){
     if (this.state.active){
@@ -27,8 +65,8 @@ export default class App extends Component {
       ]
     }else{
       return[
-        <Picker/>,
-        Button('Generate countdown', () => this.setState({ active: true }))
+        <Picker callback={(date)=>this.handleChange(date)}/>,
+        Button('Generate countdown', () => this.handleGenerate())
       ]
     }
   }bind(this)
