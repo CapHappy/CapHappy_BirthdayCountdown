68e5c52200719d94c68655b2164f3fe784b3811f
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 08:15:25 -0600

    bugfixes and styled picker

---
+++ src/components/button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const button=()=>{
+    return(
+        <button className="button">
+            Generate Countdown
+        </button>
+    )
+}
+
+export default button;
