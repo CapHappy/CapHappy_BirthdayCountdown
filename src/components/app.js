366b7cfc73078907e986852ed6e2181d8a5a6066
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 09:45:48 -0600

    finished frontend styles

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Picker from './picker';
 import Button from './button';
 import Clock from'./clock';
 import changeDate from './changeDate';
+import largeText from './largeText';
 
 export default class App extends Component {
 
@@ -18,18 +19,19 @@ export default class App extends Component {
     if (this.state.active){
       return[
         <Clock/>,
-        changeDate('Change Date', ()=> this.setState({ active: false }))
+        changeDate('Change Date', ()=> this.setState({ active: false })),
+        largeText('04/03'),
+        <label className="grid__remaning">Remaining until your 21st birthday</label>
       ]
     }else{
       return[
+        <Picker/>,
         Button('Generate countdown', () => this.setState({ active: true }))
       ]
     }
   }bind(this)
 
   render() {
-    // return(<div><Clock/></div>)
-
     return (
       <div className="grid">
         <h1 className="grid__tittle">BirthdayCountdown</h1>
@@ -39,8 +41,6 @@ export default class App extends Component {
         <div className="grid__skew-light-three-box"></div>
         <div className="grid__skew-dark-one"></div> */}
         <div className="grid__skew-dark-two-box"></div>
-
-        <Picker/>
         {this.renderItems}
         
       </div>
---
+++ src/components/largeText.js
@@ -0,0 +1,9 @@
+import react from 'React';
+
+export default LargeText = (text)=>{
+    return(
+        <div classname="large-text">
+            {text}
+        </div>
+    )
+}
